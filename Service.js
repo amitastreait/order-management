96a1908883e36b6d472f6a05d1c7b2d71380d769
vscode <xyz@gmail.com>
2025-01-13 09:24:45 +0530

    Added the Service Classes for Login & Singup

---
+++ Service.js
@@ -0,0 +1,16 @@
+/** Add the signup functionality */
+export const signup = async (email, password) => {
+  try {
+    const response = await fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error signing up:', error);
+  }
+};
