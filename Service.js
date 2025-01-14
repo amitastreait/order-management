274afdd8c951f0fb482f2dc2d8192788efd8d070
Amit Singh <cse.amitallenhouse@gmail.com>
2025-01-14 09:01:09 +0530

    Added the Exception for Login & Signup

--- Service.js
+++ Service.js
@@ -14,3 +14,19 @@ export const signup = async (email, password) => {
     console.error('Error signing up:', error);
   }
 };
+/** Add Login Exceptions */
+export const login = async (email, password) => {
+  try {
+    const response = await fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error logging in:', error);
+  }
+};
