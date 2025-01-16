51c2df229a405e182d2c93a986014108dbf6b7ad
Amit Singh <cse.amitallenhouse@gmail.com>
2025-01-16 08:47:35 +0530

    Added the payment handler

---
+++ payment/Service.js
@@ -0,0 +1,9 @@
+/** Connect with Razor pay for the payment */
+const Razorpay = require('razorpay');
+const { response } = require('express');
+const { json } = require('body-parser');
+const { json } = require('express');
+const { secret } = require('../config');
+const { response } = require('express');
+const { json } = require('body-parser');
+/** Create order and Paymet */
